2ab5ba7754f05a8d2b4750d589dd1581d39d6eef
Anavoviy <112809236+Anavoviy@users.noreply.github.com>
2022-11-11 07:58:21 +1000

    Добавление полей для CPU и добавление enum для CPU

--- CPU.cs
+++ CPU.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ComputerEnums.CPU;
+using ComputerEnums.RAM;
 
 namespace Assemblies
 {
     class CPU
     {
-        string name;
-        
+        string KeyProduct = "#0000000";
+
+        string Marking = "Product marking";
+        CPUCompany Company;
+        string Model;
+        CPUFamily Family;
+        byte Generation;
+        CPUSocket Socket;
+
+        bool BOX_OEM = false;
+        bool ThermalPaste = false;
+
+        short Cores;
+        short Threads;
+        short P_Cores;
+        short E_Cores;
+        byte L2;
+        byte L3;
+        byte TechnicalProcess;
+        string? Core;
+
+        int BaseFrequency;
+        int MaxFrequency;
+        int BaseFrecuencyE;
+        int MaxFrequencyE;
+        bool FreeMultiplier = false;
+
+        RAMType RAMType;
+        short MaxRAMCapacity;
+        byte RAMChannels;
+        int MaxRAMFrequency;
+        bool ECC_RAM = false;
+
+        short TDP;
+        byte MaxTemperature;
+
+        string BuiltInGraphics;
+
+        bool BuiltInControllerPCIE = true;
+        byte PCIELines;
+
+        ulong Cost;
+
+
     }
 }
