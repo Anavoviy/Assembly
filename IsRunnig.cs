b32416022fcbd3927b34aac1cc4d417d21b4858d
Anavoviy <soulcomp@mail.ru>
2022-11-16 07:32:30 +1000

    Добалне первичный алгоритм создания сборки ПК (комментарии в Program.cs)

--- IsRunnig.cs
+++ IsRunnig.cs
@@ -8,23 +8,8 @@ using System.Xml.Linq;
 namespace Assemblies
 {
     class IsRunning : Assembly {
-        Assembly assembly;
-
-        public IsRunning() { }
-
-        public IsRunning(Assembly ass) { this.assembly = ass; }
         
-        public bool CPU_MotherBoard() {
-            CPU cpu = this.CPU;
-            MotherBoard motherBoard = this.MotherBoard;
-
-            int countYes = 0;
-            if (cpu.Socket == motherBoard.Socket) 
-                countYes++;
-            if (cpu.Socket == motherBoard.Socket)
-                countYes++;
-
-        }
+                    
         
     }
 }
